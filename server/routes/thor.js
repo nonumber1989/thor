b8e59fb596b4f31f7359d39fba2c6b0e4b8ede19
i325776 <steven.xu01@sap.com>
2016-05-09 16:04:20 +0800

    change to my own schema generate lib

--- server/routes/thor.js
+++ server/routes/thor.js
@@ -1,43 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var jsonSchema = require('generate-schema');
+var jsonSchema = require('json-schema-utils');
 
 router.get('/', function(req, res, next) {
-var schema = jsonSchema.json('Product', [
-    {
-        "id": 2,
-        "name": "An ice sculpture",
-        "price": 12.50,
-        "tags": ["cold", "ice"],
+    var product = {
+        "id": 1,
+        "number": 1200,
+        "date": "Tue Jul 22 2008 12:11:04 GMT-0700 (Pacific Daylight Time)",
         "dimensions": {
             "length": 7.0,
             "width": 12.0,
             "height": 9.5
         },
-        "warehouseLocation": {
-            "latitude": -78.75,
-            "longitude": 20.4
-        }
-    },
-    {
-        "id": 3,
-        "name": "A blue mouse",
-        "price": 25.50,
-        "dimensions": {
-            "length": 3.1,
-            "width": 1.0,
-            "height": 1.0
-        },
-        "warehouseLocation": {
-            "latitude": 54.4,
-            "longitude": -32.7
-        }
-    }
-]);
-      res.json({
-                    type: false,
-                    data: schema
-                });
+        "users": [{
+            "name": "steven",
+            "age": 28
+        }]
+    };
+    var schema = jsonSchema.jsonSchema('Product',product);
+    res.json(schema);
 });
 
 module.exports = router;
